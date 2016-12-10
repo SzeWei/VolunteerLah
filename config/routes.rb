5cd051f062127e3f1ac66a5d9cb1b806dd5ceb55
Ian Beamish <ianbeami@gmail.com>
2016-12-10 12:28:52 +0800

    *added sign out to navbar *added search fields

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :profiles
   resources :organisation_profiles
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  resources :searches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "events#index"
+  root to: "home#index"
 
   # devise_scope :user do
   # 	delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
