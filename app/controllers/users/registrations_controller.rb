3c5e75bd3743456ee07a469f7a9fb85ef4fd5727
huatzhi <huatzhi@gmail.com>
2016-12-11 05:10:58 +0800

    removing hardcode of after_sign_up_path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
   def after_sign_up_path_for(resource)
-    '/users/edit'
+    edit_user_registration_path
   end
 
 
