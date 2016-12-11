5abfcc2f8b7b93b695069e726cca46260377868d
huatzhi <huatzhi@gmail.com>
2016-12-11 10:49:36 +0800

    a placeholder for user#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 # modal#Event
-  resources :event_volunteers
-  resources :event_details
+  resources :event_volunteers, only: [:create, :update]
+  # resources :event_details
   resources :events
 
   resources :profiles
   resources :organisation_profiles
   devise_for :users, :controllers => { registrations: "users/registrations", omniauth_callbacks: "users/omniauth_callbacks" }
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "events#index"
