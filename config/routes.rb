4a7f7f049d5e0b72f9391110cf805469e73e0b7d
Ian Beamish <ianbeami@gmail.com>
2016-12-13 17:32:55 +0800

    •homepage photo header •create event page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :organisation_profiles
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  
+  resources :home
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
