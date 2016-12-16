57fae4f01ab87bf80f91a0ba908ea4b6df10f978
SzeWei <sze_wei92@hotmail.com>
2016-12-16 15:12:41 +0800

    users profiles updated with mandatory fields and .present syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,15 @@ class User < ApplicationRecord
     end
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email    = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name     = auth.info.name
+      user.profile.avatar   = auth.info.image if user.profile.present?
+    end
+  end
+
   def build_relevant_profile
     if self.volunteer?
       self.build_profile
