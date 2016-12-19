2d3d4ee003f069607da038e019ab406320329cf4
Ian Beamish <ianbeami@gmail.com>
2016-12-19 14:15:03 +0800

    CSR

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,10 @@ Rails.application.routes.draw do
   # resources :organisation_profiles
   devise_for :users, :controllers => { registrations: "users/registrations", omniauth_callbacks: "users/omniauth_callbacks" }
   resources :users, only: [:show, :destroy]
-  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static#home"
+
+  get '/csr', to: 'static#csr' 
 
 end
